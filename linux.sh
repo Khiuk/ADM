23e4772bde71e284a33e07ca9a7fa931b47c18a9
Roman Khiuk <Khiuk.2001@gmail.com>
2024-01-31 15:16:25 +0700

    Create test sh script

---
+++ linux.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Fish install
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_10/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+sudo apt update
+sudo apt install fish
+
+# AD Prepare
+
+apt install fly-admin-ad-sssd-client
+
+# Disk Mount
+
+mkdir /mnt/network_disk
+mount -o username=inform1 //law/base/Distrib /mnt/network_disk
+
+# Kaspersky Install
+
+dpkg -i /mnt/network_disk/KESL11_11.3.0.7508\(ФСТЭК\)_643.46856491.00049-11/kesl/kesl_11.3.0-7508.cert_amd64.deb
+wait
+dpkg -i /mnt/network_disk/KESL11_11.3.0.7508\(ФСТЭК\)_643.46856491.00049-11/klnagent/klnagent64_14.0.0-4646_amd64.deb
+wait
+/opt/kaspersky/kesl/bin/kesl-setup.pl
+wait
+/opt/kaspersky/klnagent64/lib/bin/setup/postinstall.pl
+wait
